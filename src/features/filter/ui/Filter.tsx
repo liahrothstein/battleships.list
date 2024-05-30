5957a8f055d3b005111cbb954c094f0e0e2c0e67
liahrothstein <liahrothstein@gmail.com>
2024-05-30 23:57:57 +0300

    feat: add filter type, nation and level vehicles

--- src/features/filter/ui/Filter.tsx
+++ src/features/filter/ui/Filter.tsx
@@ -19,6 +19,7 @@ export function Filter() {
     const dispatch = useAppDispatch();
 
     useEffect(() => {
+        
         dispatch(setFilteredVehicles(filterVehicles(filterTags, vehicles)))
     }, [filterTags]);
 
